d81b2183defd6f01872572951af7d454489c65ae
Karl Widborg Kortelainen <karl.widkor@gmail.com>
2023-12-03 19:41:59 +0100

    📜 Add template file

---
+++ src/template.ts
@@ -0,0 +1,19 @@
+import * as fs from 'fs';
+
+function part1(): number {
+    return 0;
+}
+
+function part2(): number {
+    return 0;
+}
+
+
+function main() {
+    const file: string = fs.readFileSync("./input/d2.txt", 'utf-8');
+    let rows = file.split("\n");
+
+    let solution1 = part1();
+    let solution2 = part2();
+
+}
